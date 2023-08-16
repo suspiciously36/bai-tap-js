9649ee49748eaf075682662f15822581572f26d2
ponysbed27 <s3979682@rmit.edu.vn>
2023-08-16 07:32:41 +0700

    format code

--- js/script2.js
+++ js/script2.js
@@ -8,23 +8,18 @@ var coloredWord;
 var currentPosition = 0;
 
 function coloringWords() {
-    position = newContent.indexOf(spacing); // 5
-    console.log(position);
 
-    coloredWord = content.slice(0, currentPosition) + `<span style="color: red;">${newContent.slice(0, position)}</span>` + newContent.slice(position);
-    
-    console.log(coloredWord);
+    position = newContent.indexOf(spacing);
+    coloredWord = content.slice(0, currentPosition) + `<span style="color: red;">${newContent.slice(0, position)}</span>` + newContent.slice(position); 
     newContent = newContent.slice(position + 1);
-
-    console.log(newContent);
     currentPosition += position + 1;
-    console.log(currentPosition);
 
     if (position === -1) {
         coloredWord = content.slice(0, currentPosition) + `<span style="color: red;">${newContent.slice(0, newContent.length)}</span>`
         newContent = content; 
         currentPosition = 0;
     }
+    
     document.getElementById("paragraph").innerHTML = coloredWord;
 }
 
